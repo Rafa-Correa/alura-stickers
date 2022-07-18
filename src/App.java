fa69b1774d7aca7d5b379f0243ceb678768f9010
Rafa-Correa <88345739+Rafa-Correa@users.noreply.github.com>
2022-07-18 18:21:14 -0300

    Add files via upload

---
+++ src/App.java
@@ -0,0 +1,36 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // fazer uma conexão HTTP e buscar os tTop 250 Filmes
+        String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+
+        // pegar só os dados que interessam (título, poster, classificação)
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        
+        // exibir e manipular os dados
+        for (Map<String,String> filme : listaDeFilmes) {
+            System.out.print("Título: ");
+            System.out.println(filme.get("title"));
+            System.out.print("Poster: ");
+            System.out.println(filme.get("image"));
+            System.out.print("\u001b[45m \u001b[33m Classificação: ");
+            System.out.print("\u001b[0m");
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
+    }
+}
