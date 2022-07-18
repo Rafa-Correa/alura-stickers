aa386da5a8d2f33b968f1170be8b6a1da8bb58ba
Rafa-Correa <88345739+Rafa-Correa@users.noreply.github.com>
2022-07-18 20:29:55 -0300

    Update App.java

--- src/App.java
+++ src/App.java
@@ -23,15 +23,9 @@ public class App {
         
         // exibir e manipular os dados
         for (Map<String,String> filme : listaDeFilmes) {
-            System.out.print("\u001b[1mTítulo: ");
-            System.out.print("\u001b[0m");
-            System.out.println(filme.get("title"));
-            System.out.print("\u001b[1mPoster: ");
-            System.out.print("\u001b[0m");
-            System.out.println(filme.get("image"));
-            System.out.print("\u001b[1mClassificação: ");
-            System.out.print("\u001b[0m");
-            System.out.println(filme.get("imDbRating"));
+            System.out.println("\u001b[1mTítulo: " + "\u001b[0m" + filme.get("title"));
+            System.out.println("\u001b[1mPoster: " + "\u001b[0m" + filme.get("image"));
+            System.out.println("\u001b[1mClassificação: " + "\u001b[0m" + filme.get("imDbRating"));
             System.out.println();
         }
     }
